3f11a70e8e4fcaf66b4ce3f8ec2fba6f11780b3b
Daniel Ponte <amigan@gmail.com>
2024-08-04 15:29:07 -0400

    use new ctx

--- pkg/gordio/server/ingest.go
+++ pkg/gordio/server/ingest.go
@@ -4,13 +4,8 @@ import (
 	"context"
 
 	"dynatron.me/x/stillbox/pkg/gordio/calls"
-	"dynatron.me/x/stillbox/pkg/gordio/database"
 )
 
 func (s *Server) Ingest(ctx context.Context, call *calls.Call) {
-	// decouple this context from the one we call sinks with
-	db := database.FromCtx(ctx)
-	nctx := database.CtxWithDB(context.Background(), db)
-
-	s.sinks.EmitCall(nctx, call)
+	s.sinks.EmitCall(context.Background(), call)
 }
