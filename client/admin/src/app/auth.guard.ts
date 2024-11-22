ee7a44cd4b1e7c835c1c3d1a81fcdc4c4f79ee8a
Daniel Ponte <amigan@gmail.com>
2024-11-22 14:13:52 -0500

    rollback guard

--- client/admin/src/app/auth.guard.ts
+++ client/admin/src/app/auth.guard.ts
@@ -1,14 +1,9 @@
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 
-export class AuthGuard implements CanActivate {
-  constructor(private router: Router) { }
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (sessionStorage.getItem('jwt') != null) {
-      return true;
-    }
-
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
+export const AuthGuard: CanActivateFn = (route, state) => {
+  if (sessionStorage.getItem('jwt') == null) {
     return false;
+  } else {
+    return true;
   }
 };
