21f2ef59bf9d6169d1b925f3c3a5753824fe238b
Daniel Ponte <amigan@gmail.com>
2025-01-07 22:00:35 -0500

    Table works before matTable

--- client/stillbox/src/app/talkgroups/talkgroup-record/alert-rule-builder/alert-rule-builder.component.ts
+++ client/stillbox/src/app/talkgroups/talkgroup-record/alert-rule-builder/alert-rule-builder.component.ts
@@ -1,9 +1,28 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { AlertRule } from '../../../talkgroup';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
+import { AlertRule, AlertTime } from '../../../talkgroup';
+
+@Pipe({
+  name: 'ruleProc',
+  standalone: true,
+  pure: true,
+})
+export class AlertRulePipe implements PipeTransform {
+  transform(rule: AlertRule, args?: any): AlertTime[] {
+    let tm = new AlertRule(rule.times, rule.mult);
+    return tm.timesProc;
+  }
+}
 
 @Component({
   selector: 'alert-rule-builder',
-  imports: [],
+  imports: [AlertRulePipe],
   templateUrl: './alert-rule-builder.component.html',
   styleUrl: './alert-rule-builder.component.scss',
 })
