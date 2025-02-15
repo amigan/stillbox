771aded7e4a85e38badb7ed4a2de68a155c49f65
Daniel Ponte <amigan@gmail.com>
2025-02-15 01:02:05 -0500

    graph!

---
+++ client/stillbox/src/app/charts/charts.component.ts
@@ -0,0 +1,114 @@
+import { Component, ElementRef, Input } from '@angular/core';
+import * as d3 from 'd3';
+import { CallsService } from '../calls/calls.service';
+import { CallStatsRecord } from '../calls';
+
+@Component({
+  selector: 'chart',
+  imports: [],
+  templateUrl: './charts.component.html',
+  styleUrl: './charts.component.scss',
+})
+export class ChartsComponent {
+  @Input() interval!: string;
+  loading = true;
+  // I hate javascript so much
+  months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  constructor(
+    private elementRef: ElementRef,
+    private callsSvc: CallsService,
+  ) {}
+
+  dateFormat(d: Date): string {
+    switch (this.interval) {
+      case 'month':
+        return `${this.months[d.getMonth()]} ${d.getFullYear()}`;
+      case 'day':
+        return `${this.months[d.getMonth()]} ${d.getDate()}`;
+      case 'week':
+        return `${this.months[d.getMonth()]} ${d.getDate()}`;
+    }
+    return d.toDateString();
+  }
+
+  ngOnInit() {
+    this.callsSvc.getCallStats(this.interval).subscribe((stats) => {
+      let cMax = 0;
+      var cMin = 0;
+      let data = stats.stats.map((rec) => {
+        if (cMin == 0 && rec.count > cMin) {
+          cMin = rec.count;
+        }
+        if (rec.count < cMin) {
+          cMin = rec.count;
+        }
+        if (rec.count > cMax) {
+          cMax = rec.count;
+        }
+        return { count: rec.count, time: this.dateFormat(new Date(rec.time)) };
+      });
+      // set the dimensions and margins of the graph
+      var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+      const svg = d3
+        .select(this.elementRef.nativeElement)
+        .select('.chart')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      // X axis
+      var x = d3
+        .scaleBand()
+        .range([0, width])
+        .domain(
+          data.map(function (d) {
+            return d.time;
+          }),
+        )
+        .padding(0.2);
+      svg
+        .append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(d3.axisBottom(x))
+        .selectAll('text')
+        .attr('transform', 'translate(-10,0)rotate(-45)')
+        .style('text-anchor', 'end');
+
+      // Add Y axis
+      var y = d3.scaleLinear().domain([0, cMax]).range([height, 0]);
+      svg.append('g').call(d3.axisLeft(y));
+      svg
+        .selectAll('mybar')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('x', (d) => x(d.time)!)
+        .attr('y', function (d) {
+          return y(d.count);
+        })
+        .attr('width', x.bandwidth())
+        .attr('height', function (d) {
+          return height - y(d.count);
+        })
+        .attr('fill', function (d) {
+          return d3.interpolateTurbo((d.count - cMin) / (cMax - cMin))
+        });
+    });
+  }
+}
--- client/stillbox/src/app/home/home.component.ts
+++ client/stillbox/src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ChartsComponent } from '../charts/charts.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ChartsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
