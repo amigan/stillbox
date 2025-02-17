dcb762e56e4bf1c2be3d5c9480b5dc3067c2dc96
Daniel Ponte <amigan@gmail.com>
2025-02-17 12:03:47 -0500

    chart card

--- client/stillbox/src/app/home/home.component.ts
+++ client/stillbox/src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { ChartsComponent } from '../charts/charts.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [ChartsComponent],
+  imports: [
+    ChartsComponent,
+    MatCardModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
