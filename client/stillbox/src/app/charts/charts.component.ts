8e6684ad3cce6ed98f57aa0893575ab201c0209a
Daniel Ponte <amigan@gmail.com>
2025-02-28 17:35:09 -0500

    Improve graph

--- client/stillbox/src/app/charts/charts.component.ts
+++ client/stillbox/src/app/charts/charts.component.ts
@@ -75,21 +75,23 @@ export class ChartsComponent {
         });
         // set the dimensions and margins of the graph
         var margin = { top: 30, right: 30, bottom: 70, left: 60 },
-          width = 460 - margin.left - margin.right,
-          height = 400 - margin.top - margin.bottom;
+          widthVal = 600,
+          heightVal = 600;
         // clear the old one
         d3.select(this.elementRef.nativeElement).select('.chart').html('');
-        const svg = d3
+        let svg = d3
           .select(this.elementRef.nativeElement)
           .select('.chart')
           .append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
+          .attr('viewBox', `0 0 ${widthVal} ${heightVal}`)
           .append('g')
           .attr(
             'transform',
             'translate(' + margin.left + ',' + margin.top + ')',
           );
+
+        const width = widthVal - margin.left - margin.right;
+        const height = heightVal - margin.top - margin.bottom;
         // X axis
         var x = d3
           .scaleBand()
