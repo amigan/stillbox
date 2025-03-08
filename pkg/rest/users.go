d6e9d9aac190efb0ee4cea0b65db0e772540662e
Daniel Ponte <amigan@gmail.com>
2025-03-07 22:07:50 -0500

    Implement getuser API call

--- pkg/rest/users.go
+++ pkg/rest/users.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"net/http"
 
+	"dynatron.me/x/stillbox/pkg/users"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -18,4 +19,31 @@ func (ua *usersAPI) Subrouter() http.Handler {
 }
 
 func (ua *usersAPI) getUser(w http.ResponseWriter, r *http.Request) {
+	p := struct {
+		User string `param:"user"`
+	}{}
+
+	err := decodeParams(&p, r)
+	if err != nil {
+		wErr(w, r, badRequest(err))
+		return
+	}
+
+	ctx := r.Context()
+	ust := users.FromCtx(ctx)
+	user, err := ust.GetUser(ctx, p.User)
+	if err != nil {
+		wErr(w, r, recordNotFound(err))
+		return
+	}
+
+	response := struct {
+		UID      users.UserID `json:"uid"`
+		Username string       `json:"username"`
+	}{
+		UID:      user.ID,
+		Username: user.Username,
+	}
+
+	respond(w, r, response)
 }
