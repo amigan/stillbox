0b489886a53fe923f0bb6311d3906b9f27e9946b
Daniel Ponte <amigan@gmail.com>
2025-07-29 08:52:07 -0400

    Split API keys into own subrouter

--- pkg/rest/users.go
+++ pkg/rest/users.go
@@ -3,10 +3,7 @@ package rest
 import (
 	"net/http"
 	"net/netip"
-	"time"
 
-	"dynatron.me/x/stillbox/internal/common"
-	"dynatron.me/x/stillbox/internal/forms"
 	"dynatron.me/x/stillbox/internal/jsontypes"
 	"dynatron.me/x/stillbox/pkg/users"
 	"github.com/go-chi/chi/v5"
@@ -19,35 +16,8 @@ func (ua *usersAPI) Subrouter() http.Handler {
 	r := chi.NewMux()
 
 	r.Get("/{user}", ua.getUser)
-	r.Post("/createAPIkey", ua.createAPIkey)
 
 	return r
-}
-
-func (ua *usersAPI) createAPIkey(w http.ResponseWriter, r *http.Request) {
-	input := struct {
-		Owner     *int            `json:"owner"`
-		Name      *string         `json:"name"`
-		ExpiresAt *jsontypes.Time `json:"expiresAt"`
-		Disabled  *bool           `json:"disabled"`
-	}{}
-
-	err := forms.Unmarshal(r, &input, forms.WithTag("json"), forms.WithAcceptBlank(), forms.WithOmitEmpty())
-	if err != nil {
-		wErr(w, r, badRequest(err))
-		return
-	}
-
-	ctx := r.Context()
-	ust := users.FromCtx(ctx)
-	key, err := ust.CreateAPIKey(ctx, (*users.UserID)(input.Owner), input.Name, (*time.Time)(input.ExpiresAt), common.ZeroIfNil(input.Disabled))
-	if err != nil {
-		wErr(w, r, autoError(err))
-		return
-	}
-
-	respond(w, r, key)
-
 }
 
 func (ua *usersAPI) getUser(w http.ResponseWriter, r *http.Request) {
