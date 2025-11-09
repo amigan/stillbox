48245bfef3ccbb39f12a6769ff0e636b44408ce7
Daniel Ponte <amigan@gmail.com>
2025-11-09 11:12:06 -0500

    Avatar

---
+++ client/stillbox/src/app/user/avatar/avatar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'avatar',
+  imports: [],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.scss',
+})
+export class AvatarComponent {
+  constructor(public userSvc: UserService) {}
+
+  getInitials(): string {
+    let uname = this.userSvc.selfUser()?.realName;
+    if (uname == null || uname == undefined) {
+      uname = this.userSvc.selfUser()?.username;
+    } else {
+      let names = uname!.split(' ');
+      return names[0][0].toUpperCase() + names[1][0].toUpperCase();
+    }
+    return uname![0].toUpperCase() + uname![1].toUpperCase();
+  }
+}
