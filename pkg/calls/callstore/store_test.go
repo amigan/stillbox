ae10d1bf79c4f17e75386d65e1fcc930a797539a
Daniel Ponte <amigan@gmail.com>
2026-01-23 23:34:38 -0500

    Split integration tests out

--- pkg/calls/callstore/store_test.go
+++ pkg/calls/callstore/store_test.go
@@ -1,5 +1,3 @@
-//go:build integration
-
 package callstore_test
 
 import (
@@ -7,7 +5,6 @@ import (
 	"sync"
 	"testing"
 
-	"dynatron.me/x/stillbox/internal/testutil"
 	"dynatron.me/x/stillbox/pkg/calls/callstore"
 	"dynatron.me/x/stillbox/pkg/config"
 	"dynatron.me/x/stillbox/pkg/database"
@@ -16,7 +13,6 @@ import (
 	"dynatron.me/x/stillbox/pkg/talkgroups/tgstore"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 )
 
 var mockRegisterBackend sync.Once
@@ -43,35 +39,4 @@ func setupMockDBStoreB(ctx context.Context, db database.Store, storeCfg config.C
 	met := metrics.NewNoOp()
 	tgc := tgstore.NewCache(db, met)
 	return callstore.NewStore(ctx, db, tgc, met, storeCfg, partCfg)
-}
-
-type DBTestSuite struct {
-	suite.Suite
-	db    *testutil.DB
-	store callstore.Store
-	tgs   tgstore.Store
-}
-
-func (s *DBTestSuite) TearDownTest() {
-	s.db.Cleanup()
-}
-
-func NewDBTestSuite(ctx context.Context, t *testing.T, storeCfg config.CallStorage, partCfg testutil.PartConfig, stOpt ...testutil.DBOpt) (*DBTestSuite, context.Context) {
-	met := metrics.NewNoOp()
-	db := testutil.NewDB(partCfg, stOpt...)
-	t.Logf("schema %s", db.SchemaName)
-	tgc := tgstore.NewCache(db, met)
-	st, err := callstore.NewStore(ctx, db, tgc, met, storeCfg, db.PartConfig, callstore.WithNow(callstore.NowFunc(db.NowFunc())))
-	require.NoError(t, err)
-
-	dbts := &DBTestSuite{
-		db:    db,
-		store: st,
-	}
-
-	ctx = database.CtxWithDB(ctx, db)
-	ctx = tgstore.CtxWithStore(ctx, tgc)
-	ctx = callstore.CtxWithStore(ctx, st)
-
-	return dbts, ctx
 }
---
+++ pkg/calls/callstore/suite_test.go
@@ -0,0 +1,47 @@
+//go:build integration
+package callstore_test
+
+import (
+	"context"
+	"testing"
+
+	"dynatron.me/x/stillbox/internal/testutil"
+	"dynatron.me/x/stillbox/pkg/calls/callstore"
+	"dynatron.me/x/stillbox/pkg/config"
+	"dynatron.me/x/stillbox/pkg/database"
+	"dynatron.me/x/stillbox/pkg/metrics"
+	"dynatron.me/x/stillbox/pkg/talkgroups/tgstore"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type DBTestSuite struct {
+	suite.Suite
+	db    *testutil.DB
+	store callstore.Store
+	tgs   tgstore.Store
+}
+
+func (s *DBTestSuite) TearDownTest() {
+	s.db.Cleanup()
+}
+
+func NewDBTestSuite(ctx context.Context, t *testing.T, storeCfg config.CallStorage, partCfg testutil.PartConfig, stOpt ...testutil.DBOpt) (*DBTestSuite, context.Context) {
+	met := metrics.NewNoOp()
+	db := testutil.NewDB(partCfg, stOpt...)
+	t.Logf("schema %s", db.SchemaName)
+	tgc := tgstore.NewCache(db, met)
+	st, err := callstore.NewStore(ctx, db, tgc, met, storeCfg, db.PartConfig, callstore.WithNow(callstore.NowFunc(db.NowFunc())))
+	require.NoError(t, err)
+
+	dbts := &DBTestSuite{
+		db:    db,
+		store: st,
+	}
+
+	ctx = database.CtxWithDB(ctx, db)
+	ctx = tgstore.CtxWithStore(ctx, tgc)
+	ctx = callstore.CtxWithStore(ctx, st)
+
+	return dbts, ctx
+}
