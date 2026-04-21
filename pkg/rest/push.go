b635260dced91a7ed03e4a8a6f16ecb7c788d28b
Daniel Ponte <amigan@gmail.com>
2026-04-21 08:02:16 -0400

    Add singleton methods

--- pkg/rest/push.go
+++ pkg/rest/push.go
@@ -6,6 +6,7 @@ import (
 
 	"dynatron.me/x/stillbox/internal/forms"
 	"dynatron.me/x/stillbox/pkg/notify/push"
+	"dynatron.me/x/stillbox/pkg/talkgroups"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -19,54 +20,110 @@ func (pa *pushAPI) Subrouter() http.Handler {
 	r.Get("/vapid", pa.getVAPIDPubkey)
 	r.Post("/subscribe", pa.subscribeWebPush)
 	r.Get("/subscriptions", pa.getSubscriptions)
-	r.Delete("/subscriptions", pa.unsubscribe)
-	r.Post("/subscriptions", pa.addSubscribe)
+	r.Delete("/subscriptions", pa.unsubscribe(false))
+	r.Post("/subscriptions", pa.subscribe(false))
+	r.Put("/subscriptions/{system:[0-9]+}", pa.subscribe(true))
+	r.Put("/subscriptions/{system:[0-9]+}/{id:[0-9]+}", pa.subscribe(true))
+	r.Delete("/subscriptions/{system:[0-9]+}", pa.unsubscribe(true))
+	r.Delete("/subscriptions/{system:[0-9]+}/{id:[0-9]+}", pa.unsubscribe(true))
 
 	return r
 }
 
-func (pa *pushAPI) addSubscribe(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
-	var subSet push.SubscriptionSet
-	err := forms.Unmarshal(r, &subSet, forms.WithTag("json"), forms.WithAcceptBlank(), forms.WithOmitEmpty())
+func (pa *pushAPI) subsetFromParams(r *http.Request) (subSet push.SubscriptionSet, err error) {
+	var par tgParams
+	err = decodeParams(&par, r)
 	if err != nil {
-		wErr(w, r, badRequestErrText(err))
 		return
 	}
 
-	err = pa.push.Subscribe(ctx, &subSet)
-	if err != nil {
-		wErr(w, r, autoError(err))
-		return
+	if par.System != nil {
+		if par.ID != nil {
+			subSet.Talkgroups = talkgroups.IDs{
+				talkgroups.ID{
+					System:    uint32(*par.System),
+					Talkgroup: uint32(*par.ID),
+				},
+			}
+		} else {
+			subSet.Systems = []int32{int32(*par.System)}
+		}
+	} else { // can't happen
+		return subSet, ErrMissingTGSys
 	}
 
-	w.WriteHeader(http.StatusNoContent)
-
+	return subSet, nil
 }
 
-func (pa *pushAPI) unsubscribe(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+func (pa *pushAPI) subscribe(singleton bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
 
-	var subSet push.SubscriptionSet
-	var isEmpty bool
-	err := forms.Unmarshal(r, &subSet, forms.WithTag("json"), forms.WithAcceptBlank(), forms.WithOmitEmpty(), forms.WithAcceptEmptyBodyResult(&isEmpty))
-	if err != nil {
-		wErr(w, r, badRequestErrText(err))
-		return
+		var err error
+		var subSet push.SubscriptionSet
+
+		switch singleton {
+		case true:
+			subSet, err = pa.subsetFromParams(r)
+			if err != nil {
+				wErr(w, r, autoError(err))
+				return
+			}
+		case false:
+			err := forms.Unmarshal(r, &subSet, forms.WithTag("json"), forms.WithAcceptBlank(), forms.WithOmitEmpty())
+			if err != nil {
+				wErr(w, r, badRequestErrText(err))
+				return
+			}
+		}
+
+		err = pa.push.Subscribe(ctx, &subSet)
+		if err != nil {
+			wErr(w, r, autoError(err))
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+
 	}
+}
 
-	if isEmpty {
-		subSet.UnsubscribeAll = &isEmpty // it's already true
+func (pa *pushAPI) unsubscribe(singleton bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		var subSet push.SubscriptionSet
+		var err error
+		var isEmpty bool
+
+		switch singleton {
+		case true:
+			subSet, err = pa.subsetFromParams(r)
+			if err != nil {
+				wErr(w, r, autoError(err))
+				return
+			}
+		case false:
+			// if it's empty, delete all subscriptions.
+			err := forms.Unmarshal(r, &subSet, forms.WithTag("json"), forms.WithAcceptBlank(), forms.WithOmitEmpty(), forms.WithAcceptEmptyBodyResult(&isEmpty))
+			if err != nil {
+				wErr(w, r, badRequestErrText(err))
+				return
+			}
+		}
+
+		if isEmpty {
+			subSet.UnsubscribeAll = &isEmpty // it's already true
+		}
+
+		err = pa.push.Unsubscribe(ctx, &subSet)
+		if err != nil {
+			wErr(w, r, autoError(err))
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
 	}
-
-	err = pa.push.Unsubscribe(ctx, &subSet)
-	if err != nil {
-		wErr(w, r, autoError(err))
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
 }
 
 func (pa *pushAPI) getSubscriptions(w http.ResponseWriter, r *http.Request) {
